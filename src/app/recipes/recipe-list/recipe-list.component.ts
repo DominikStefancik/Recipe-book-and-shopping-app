cfe49b060b8eb96c0322edc6b85582083d3437c9
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2017-11-19 22:45:30 +0100

    Prepare a skeleton of recipes component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../domain/recipe';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "Simply a test recipe, nothing else", "https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg"),
+    new Recipe("A test recipe", "Simply a test recipe, nothing else", "https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg"),
+    new Recipe("A test recipe", "Simply a test recipe, nothing else", "https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg"),
+    new Recipe("A test recipe", "Simply a test recipe, nothing else", "https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg")
+  ];
 
   constructor() { }
 
