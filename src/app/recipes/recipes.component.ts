466e05dad6d196906a86376ce127a3c78cd2022c
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2017-12-03 23:32:41 +0100

    Connect the recipe list with the recipe detail so when clicking on a recipe from the list its detail is shown

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../domain/recipe';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+  }
 }
