cd90e55cb2077c06fa252d96079bc3d857164edd
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2017-12-04 21:41:14 +0100

    Enble adding a new ingredient into the shopping list

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild("nameInput") nameInput: ElementRef;
+  @ViewChild("amountInput") amountInput: ElementRef;
+  @Output() ingredientAdded: EventEmitter<{name: string, amount: number}>;
 
-  constructor() { }
+  constructor() {
+    this.ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  }
 
   ngOnInit() {
   }
 
+  onAddClicked():void {
+    this.ingredientAdded.emit({
+      name: this.nameInput.nativeElement.value,
+      amount: this.amountInput.nativeElement.value
+    });
+  }
 }
