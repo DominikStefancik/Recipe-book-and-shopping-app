01c9239f00dec52aa339a426e22f87e0c02e6baa
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2017-12-08 22:38:32 +0100

    Add a directive to open dropdown menus

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit {
+  private isDropdownOpen: boolean;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.isDropdownOpen = false;
+  }
+
+  @HostListener('click') onDropdownClick() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    if (this.isDropdownOpen) {
+      this.renderer.addClass(this.elementRef.nativeElement, "open");
+    } else {
+      this.renderer.removeClass(this.elementRef.nativeElement, "open");
+    }
+  }
+}
