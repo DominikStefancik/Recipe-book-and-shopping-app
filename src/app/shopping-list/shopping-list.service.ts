db72b66584d2b598f1bf9b7be64b9da8c3fb57d4
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2017-12-17 16:12:10 +0100

    Use "push notifications" to update ingredients list after adding a new ingredient

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,17 @@
+import { EventEmitter } from '@angular/core';
+
 import { Ingredient } from '../domain/ingredient';
 
 export class ShoppingListService {
+  ingredientsChanged: EventEmitter<Ingredient[]>;
   private ingredients: Ingredient[] = [
     new Ingredient("Apple", 5),
     new Ingredient("Orange", 10)
   ];
+
+  constructor() {
+    this.ingredientsChanged = new EventEmitter<Ingredient[]>();
+  }
 
   getIngredients():Ingredient[] {
     return this.ingredients.slice();
@@ -12,5 +19,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient:Ingredient):void {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
