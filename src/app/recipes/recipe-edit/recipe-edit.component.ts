5845d24a2267341611719fd3c98f41fee88f104f
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-01-21 01:18:54 +0100

    Create a form for editing a recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../../domain/recipe';
@@ -13,9 +14,11 @@ export class RecipeEditComponent implements OnInit {
   recipe: Recipe;
   id: number;
   editMode: boolean = false;
+  recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-              private recipesService: RecipesService) {}
+              private recipesService: RecipesService,
+              private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.route.params
@@ -26,6 +29,36 @@ export class RecipeEditComponent implements OnInit {
         if (this.editMode) {
           this.recipe = this.recipesService.getRecipe(this.id);
         }
+
+        this.initForm();
       });
+  }
+
+  private initForm(): void {
+    const recipeName = this.editMode ? this.recipe.name : '';
+    const recipeImageUrl = this.editMode ? this.recipe.imageUrl : '';
+    const recipeDescription = this.editMode ? this.recipe.description : '';
+    const recipeIngredients =
+            this.editMode ? this.recipe.ingredients
+                                       .map(ingredient => this.createIngredientRow(ingredient.name, ingredient.amount))
+                          : [];
+
+    this.recipeForm = this.formBuilder.group({
+      'name' : this.formBuilder.control(recipeName),
+      'imageUrl' : this.formBuilder.control(recipeImageUrl),
+      'description' : this.formBuilder.control(recipeDescription),
+      'ingredients' : this.formBuilder.array(recipeIngredients)
+    });
+  }
+
+  private createIngredientRow(name: string, amount: number): FormGroup {
+    return this.formBuilder.group({
+      'name' : this.formBuilder.control(name),
+      'amount' : this.formBuilder.control(amount)
+    });
+  }
+
+  onSave(): void {
+    console.log(this.recipeForm);
   }
 }
