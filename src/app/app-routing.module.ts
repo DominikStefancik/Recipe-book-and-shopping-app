33dca8f65e589c905e0a048df2061b5834cbc922
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-02-10 22:04:45 +0100

    Create a separate module for recipes section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
-import { AuthGuard } from "./auth/auth-guard.service";
 import { SignInComponent } from "./auth/sign-in/sign-in.component";
 import { SignUpComponent } from "./auth/sign-up/sign-up.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEmptyDetailComponent } from "./recipes/recipe-detail/recipe-empty-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
   // "pathMatch" ensures that redirection happens only if full path is empty
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
-  { path: "recipes", component: RecipesComponent, children: [
-    { path: "", component: RecipeEmptyDetailComponent },
-    { path: "new", component: RecipeEditComponent, canActivate: [AuthGuard] },
-    { path: ":id", component: RecipeDetailComponent },
-    { path: ":id/edit", component: RecipeEditComponent, canActivate: [AuthGuard] }
-  ] },
   { path: "shopping-list", component: ShoppingListComponent },
   { path: "signup", component: SignUpComponent },
   { path: "signin", component: SignInComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
+import { RecipesModule } from "./recipes/recipes.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthGuard } from "./auth/auth-guard.service";
@@ -10,15 +11,9 @@ import { AuthService } from "./auth/auth.service";
 import { HeaderComponent } from "./header/header.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingListEditComponent } from "./shopping-list/shopping-list-edit/shopping-list-edit.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEmptyDetailComponent } from "./recipes/recipe-detail/recipe-empty-detail.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { RecipesService } from "./recipes/recipes.service";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipesBackendService } from "./recipes/recipes-backend.service";
 import { SignInComponent } from "./auth/sign-in/sign-in.component";
 import { SignUpComponent } from "./auth/sign-up/sign-up.component";
@@ -29,13 +24,7 @@ import { SignUpComponent } from "./auth/sign-up/sign-up.component";
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipeEmptyDetailComponent,
     DropdownDirective,
-    RecipeEditComponent,
     SignInComponent,
     SignUpComponent
   ],
@@ -43,8 +32,8 @@ import { SignUpComponent } from "./auth/sign-up/sign-up.component";
     BrowserModule,
     HttpModule,
     FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipesModule
   ],
   providers: [
     ShoppingListService,
