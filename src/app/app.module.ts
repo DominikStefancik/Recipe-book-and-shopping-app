b53cbe0ad151a58e147ca6080e97fa8522b79a8e
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-02-11 17:50:37 +0100

    Remove unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
-import { FormsModule } from "@angular/forms";
 
 import { RecipesModule } from "./recipes/recipes.module";
 import { AppRoutingModule } from "./app-routing.module";
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { CommonModule } from "@angular/common";
 
 import { AuthRoutingModule } from "./auth-routing.module";
 import { SignInComponent } from "./sign-in/sign-in.component";
@@ -12,7 +11,6 @@ import { SignUpComponent } from "./sign-up/sign-up.component";
     SignUpComponent
   ],
   imports: [
-    CommonModule,
     FormsModule,
     AuthRoutingModule
   ]
