3a087f5bd407c09c7d0ff8972cca0cc63c7232dd
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-02-24 00:13:09 +0100

    Don't hardcode query parameter in the backend service URL

--- src/app/recipes/recipes-backend.service.ts
+++ src/app/recipes/recipes-backend.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import "rxjs/add/operator/map";
 
 import { Recipe } from "../domain/recipe";
 import { RecipesService } from "./recipes.service";
 import { AuthService } from "../auth/auth.service";
 
-const firebaseBackendUrl = "https://recipe-book-dc112.firebaseio.com/recipes.json?auth=";
+const firebaseBackendUrl = "https://recipe-book-dc112.firebaseio.com/recipes.json";
 
 @Injectable()
 export class RecipesBackendService {
@@ -22,7 +22,7 @@ export class RecipesBackendService {
     // will be returned. Hence we can call the request like this
     // 'this.http.get<Recipe[]>(firebaseBackendUrl + token)'
 
-    return this.http.get(firebaseBackendUrl + token)
+    return this.http.get(firebaseBackendUrl, { params : new  HttpParams().set("auth", token) })
       .map((recipes: Recipe[]) => {
         recipes.forEach(recipe => {
           if (!recipe["ingredients"]) {
@@ -39,6 +39,6 @@ export class RecipesBackendService {
   saveRecipes() {
     const token = this.authService.getToken();
     const recipes = this.recipesService.getRecipes();
-    return this.http.put(firebaseBackendUrl + token, recipes);
+    return this.http.put(firebaseBackendUrl, recipes, { params : new  HttpParams().set("auth", token) });
   }
 }
